ecb8c92b1c176456b20ead86b7a688b7a2ce0b6b
aneacsu <aneacsu@marktplaats.nl>
2014-09-07 14:25:36 +0200

    add filtering

--- www/js/Paskamer-client.js
+++ www/js/Paskamer-client.js
@@ -23,7 +23,7 @@ function ($, _, SlideMenu, ImageUploader, ImageLoader, Roulette) {
             var menu = new SlideMenu();
             menu.init();
 
-            this.initCapiRandomButton();
+            this.initRandomButton();
             this.initCreateButton();
             this.initCloseCreateFormButton();
             this.initConfirmCreateFormButton();
@@ -80,70 +80,23 @@ function ($, _, SlideMenu, ImageUploader, ImageLoader, Roulette) {
             });
         },
 
-        initCapiRandomButton: function () {
+        initRandomButton: function () {
             $('#random-items-button').click(_.bind(function () {
                 var params = {};
 
-                $.ajax({
-                    url: 'http://149.210.145.128:8080/random',
-                    success: function (data) {
-                        console.log(data);
-                        if ($('.lock-top').attr('data-locked') == 'false') {
-                            $('#top-image').attr('src', data[0]['imageUrl']);
-                        }
-                        if ($('.lock-center').attr('data-locked') == 'false') {
-                            $('#center-image').attr('src', data[1]['imageUrl']);
-                        }
-                        if ($('.lock-bottom').attr('data-locked') == 'false') {
-                            $('#bottom-image').attr('src', data[2]['imageUrl']);
-                        }
-                        this.imageLoader.update(params);
-                    }
-                });
+                if ($('.lock-top').attr('data-locked') == 'false') {
+                    params["top"] = $('.select-menu-type-top :selected').attr('value');
+                }
+                if ($('.lock-center').attr('data-locked') == 'false') {
+                    params['center'] = $('.select-menu-type-center :selected').attr('value');
+                }
+                if ($('.lock-bottom').attr('data-locked') == 'false') {
+                    params['bottom'] = $('.select-menu-type-bottom :selected').attr('value');
+                }
 
-
+                this.imageLoader.update(params);
             }, this));
         },
-
-//        initCapiRandomButton: function () {
-//            $('#random-items-button').click(_.bind(function () {
-//                var params = {};
-//
-//                $.ajax({
-//                    url: 'http://149.210.145.128:9090/random',
-//                    data: 'json',
-//                    success: function (data) {
-//                        if ($('.lock-top').attr('data-locked') == 'false') {
-//                            params["top"] = {
-//                                'type': $('.select-menu-type-top :selected').attr('value'),
-//                                'size': '',
-//                                'condition': '',
-//                                'color': ''
-//                            };
-//                        }
-//                        if ($('.lock-center').attr('data-locked') == 'false') {
-//                            params['center'] = {
-//                                'type': $('.select-menu-type-center :selected').attr('value'),
-//                                'size': '',
-//                                'condition': '',
-//                                'color': ''
-//                            };
-//                        }
-//                        if ($('.lock-bottom').attr('data-locked') == 'false') {
-//                            params['bottom'] = {
-//                                'type': $('.select-menu-type-bottom :selected').attr('value'),
-//                                'size': '',
-//                                'condition': '',
-//                                'color': ''
-//                            };
-//                        }
-//                        this.imageLoader.update(params);
-//                    }
-//                });
-//
-//
-//            }, this));
-//        },
 
         initCreateButton: function () {
             $('#create-button').click(function () {
--- www/js/widgets/ImageLoader.js
+++ www/js/widgets/ImageLoader.js
@@ -1,76 +1,76 @@
 define([
-    'jquery',
-    'underscore'
-],
+        'jquery',
+        'underscore'
+    ],
 
-function($, _) {
-    'use strict';
+    function ($, _) {
+        'use strict';
 
-    var ImageLoader = function(slots) {
-        this.slots = slots;
-        _.each(this.slots, _.bind(function(slot) {
-            slot.load(_.bind(this.show, slot));
-        }, this));
-    };
+        var ImageLoader = function (slots) {
+            this.slots = slots;
+            _.each(this.slots, _.bind(function (slot) {
+                slot.load(_.bind(this.show, slot));
+            }, this));
+        };
 
-    _.extend(
-        ImageLoader.prototype, {
-            show: function() {
-                this
-                    .velocity({
-                        translateX: "170px"
-                    }, 10)
-                    .velocity({
-                        translateX: "0",
-                        opacity: 1
+        _.extend(
+            ImageLoader.prototype, {
+                show: function () {
+                    this
+                        .velocity({
+                            translateX: "170px"
+                        }, 10)
+                        .velocity({
+                            translateX: "0",
+                            opacity: 1
+                        });
+                },
+
+                update: function (params) {
+                    $.ajax({
+                        type: 'GET',
+                        url: 'http://149.210.145.128:8080/random',
+                        data: params,
+                        context: this,
+                        success: function (data) {
+                            if (data['top'] !== undefined) {
+                                this.slots.top
+                                    .velocity({
+                                        translateX: "-170px",
+                                        opacity: 0
+                                    });
+
+                                this.slots.top.attr('src', data['top']['imageUrl']);
+                                $('#top-price').text(data['top']['price']);
+                            }
+
+                            if (data['center'] !== undefined) {
+                                this.slots.center
+                                    .velocity({
+                                        translateX: "-170px",
+                                        opacity: 0
+                                    });
+
+                                this.slots.center.attr('src', data['center']['imageUrl']);
+                                $('#center-price').text(data['center']['price']);
+                            }
+
+                            if (data['bottom'] !== undefined) {
+                                this.slots.bottom
+                                    .velocity({
+                                        translateX: "-170px",
+                                        opacity: 0
+                                    });
+
+                                this.slots.bottom.attr('src', data['bottom']['imageUrl']);
+                                $('#bottom-price').text(data['bottom']['price']);
+                            }
+                        },
+                        dataType: 'json'
                     });
-            },
-            
-            update: function(params) {
-                $.ajax({
-                    type: 'POST',
-                    url: '/randomImage',
-                    data: params,
-                    context: this,
-                    success: function(data) {
-                        if (data['top'] !== undefined) {
-                            this.slots.top
-                                .velocity({
-                                    translateX: "-170px",
-                                    opacity: 0
-                                });
-                            
-                            this.slots.top.attr('src', data['top']['imageUrl']);
-                            $('#top-price').text(data['top']['price']);
-                        }
-                        
-                        if (data['center'] !== undefined) {
-                            this.slots.center
-                                .velocity({
-                                    translateX: "-170px",
-                                    opacity: 0
-                                });
-
-                            this.slots.center.attr('src', data['center']['imageUrl']);
-                            $('#center-price').text(data['center']['price']);
-                        }
-
-                        if (data['bottom'] !== undefined) {
-                            this.slots.bottom
-                                .velocity({
-                                    translateX: "-170px",
-                                    opacity: 0
-                                });
-
-                            this.slots.bottom.attr('src', data['bottom']['imageUrl']);
-                            $('#bottom-price').text(data['bottom']['price']);
-                        }
-                    },
-                    dataType: 'json'
-                });
+                }
             }
-        }
-    );
+        );
 
-    return ImageLoader;
-});
+        return ImageLoader;
+    });
