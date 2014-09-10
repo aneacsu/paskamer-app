8e92cfdec3bd42cf2cc552bd096ac527f4fde148
ltatarov <ltatarov@ebay.com>
2014-09-10 22:41:25 +0200

    remove slide

--- www/js/widgets/ImageLoader.js
+++ www/js/widgets/ImageLoader.js
@@ -33,29 +33,29 @@ define([
                         data: params,
                         context: this,
                         success: function (data) {
-                            this.slots.top
-                                .velocity({
-                                    translateX: "-170px",
-                                    opacity: 0
-                                });
+//                            this.slots.top
+//                                .velocity({
+//                                    translateX: "-170px",
+//                                    opacity: 0
+//                                });
 
                             this.slots.top.attr('src', data[0]['imageUrl']);
                             $('#top-price').text(data[0]['price']);
 
-                            this.slots.center
-                                .velocity({
-                                    translateX: "-170px",
-                                    opacity: 0
-                                });
+//                            this.slots.center
+//                                .velocity({
+//                                    translateX: "-170px",
+//                                    opacity: 0
+//                                });
 
                             this.slots.center.attr('src', data[1]['imageUrl']);
                             $('#center-price').text(data[1]['price']);
 
-                            this.slots.bottom
-                                .velocity({
-                                    translateX: "-170px",
-                                    opacity: 0
-                                });
+//                            this.slots.bottom
+//                                .velocity({
+//                                    translateX: "-170px",
+//                                    opacity: 0
+//                                });
 
                             this.slots.bottom.attr('src', data[2]['imageUrl']);
                             $('#bottom-price').text(data[2]['price']);
