debb6ffd7d28bb1730914c49a05d8757d8b3d7e2
aneacsu <aneacsu@marktplaats.nl>
2014-09-15 13:39:29 +0200

    increase image size

--- www/js/savedOutfits.js
+++ www/js/savedOutfits.js
@@ -3,72 +3,72 @@ var savedOutfits = {};
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"] = [];
 
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/Nzg2WDcxMA==/z/EdYAAMXQeW5Ta1Id/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/2dQAAOSwPe1UDf1C/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/FIQAAOSwGvhUDa94/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/Nzg2WDcxMA==/z/EdYAAMXQeW5Ta1Id/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/2dQAAOSwPe1UDf1C/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/FIQAAOSwGvhUDa94/$_82.JPG',
     'price': 31.7,
     'name': 'Sexy blue'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg3Njg=/z/M1YAAOSwQItUDfyD/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/MTAyNFg2ODA=/z/W3EAAOSwDk5T2KmN/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/MTkwWDIxNw==/z/~CwAAOxyItRTcfkB/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg3Njg=/z/M1YAAOSwQItUDfyD/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/MTAyNFg2ODA=/z/W3EAAOSwDk5T2KmN/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/MTkwWDIxNw==/z/~CwAAOxyItRTcfkB/$_82.JPG',
     'price': 34.5,
     'name': 'Love pink'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg1NzY=/z/EJUAAOSwPe1UDfZ1/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/wvcAAOSw7NNUDgR-/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/cd0AAOSwRLZUDgEB/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg1NzY=/z/EJUAAOSwPe1UDfZ1/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/wvcAAOSw7NNUDgR-/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/cd0AAOSwRLZUDgEB/$_82.JPG',
     'price': 25,
     'name': 'Flowers'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NTAxWDY3MA==/z/InYAAOSwQItUDfMP/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/wvcAAOSw7NNUDgR-/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/MjIzWDI5Nw==/z/EAcAAOSw-jhUDZhf/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NTAxWDY3MA==/z/InYAAOSwQItUDfMP/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/wvcAAOSw7NNUDgR-/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/MjIzWDI5Nw==/z/EAcAAOSw-jhUDZhf/$_82.JPG',
     'price': 41,
     'name': 'Smart casual'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg3Njg=/z/1yEAAOSwQItUDflX/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/MTAyNFg1NzU=/z/wx0AAOSw7NNT6xGi/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/MZMAAOSw7NNUDfH4/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg3Njg=/z/1yEAAOSwQItUDflX/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/MTAyNFg1NzU=/z/wx0AAOSw7NNT6xGi/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/MZMAAOSw7NNUDfH4/$_82.JPG',
     'price': 32,
     'name': 'Mint'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/p68AAOSwGvhUDfxf/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NTAwWDM3NA==/z/y1EAAOSwRLZUDfMP/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/cd0AAOSwRLZUDgEB/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/p68AAOSwGvhUDfxf/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NTAwWDM3NA==/z/y1EAAOSwRLZUDfMP/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/cd0AAOSwRLZUDgEB/$_82.JPG',
     'price': 44,
     'name': 'Summer fresh'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NTUwWDQxMw==/z/r~MAAOSwDZxTznNO/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/MTIwWDEyMA==/z/7~oAAOSwh6xTs7Fq/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/wigAAOSw7NNUDWrl/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NTUwWDQxMw==/z/r~MAAOSwDZxTznNO/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/MTIwWDEyMA==/z/7~oAAOSwh6xTs7Fq/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/wigAAOSw7NNUDWrl/$_82.JPG',
     'price': 27.9,
     'name': 'Fresh daisy'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFgxMDI0/z/hbQAAOSwxH1UDdNm/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/-7kAAOSwnDZUDcBj/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/Mzc0WDQ5OA==/z/M9AAAOSwEK9UDcax/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFgxMDI0/z/hbQAAOSwxH1UDdNm/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/-7kAAOSwnDZUDcBj/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/Mzc0WDQ5OA==/z/M9AAAOSwEK9UDcax/$_82.JPG',
     'price': 38,
     'name': 'Navy'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MzY1WDQyNQ==/z/V1YAAOSwxCxT0zn-/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/MTAyNFg4NzU=/z/BPMAAOSwDk5UDc3x/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/MTAyNFg2ODM=/z/A6sAAOSwGvhUDdNL/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MzY1WDQyNQ==/z/V1YAAOSwxCxT0zn-/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/MTAyNFg4NzU=/z/BPMAAOSwDk5UDc3x/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/MTAyNFg2ODM=/z/A6sAAOSwGvhUDdNL/$_82.JPG',
     'price': 21.3,
     'name': 'Working day'
 });
 savedOutfits["top=t-shirt&center=broeken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NTYwWDUyMA==/z/Gy8AAOSwVFlUDVAp/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NDgwWDY0MA==/z/u0MAAOSwq7JUDdRJ/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/ODAwWDYwMA==/z/re0AAOSwfcVUDeSN/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NTYwWDUyMA==/z/Gy8AAOSwVFlUDVAp/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NDgwWDY0MA==/z/u0MAAOSwq7JUDdRJ/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/ODAwWDYwMA==/z/re0AAOSwfcVUDeSN/$_82.JPG',
     'price': 55.4,
     'name': 'Hush puppy'
 });
@@ -79,37 +79,37 @@ savedOutfits["top=t-shirt&center=broeken&bottom=laarzen"] = [];
 savedOutfits["top=t-shirt&center=rokken&bottom=schoenen"] = [];
 
 savedOutfits["top=t-shirt&center=rokken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/p68AAOSwGvhUDfxf/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NTUwWDQxMw==/z/mvcAAOSwEK9TznnF/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NjAwWDgwMA==/z/dqsAAOSwEK9UDe50/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/p68AAOSwGvhUDfxf/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NTUwWDQxMw==/z/mvcAAOSwEK9TznnF/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NjAwWDgwMA==/z/dqsAAOSwEK9UDe50/$_82.JPG',
     'price': 35,
     'name': 'Chic'
 });
 savedOutfits["top=t-shirt&center=rokken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NDM2WDU3NQ==/z/kZAAAOSwd4tUDYlD/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/XGMAAOSwEK9UDdBO/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/MjkzWDM2Nw==/z/1dIAAOxyBjBTV7Ki/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NDM2WDU3NQ==/z/kZAAAOSwd4tUDYlD/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/XGMAAOSwEK9UDdBO/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/MjkzWDM2Nw==/z/1dIAAOxyBjBTV7Ki/$_82.JPG',
     'price': 28,
     'name': 'Hippie shake'
 });
 savedOutfits["top=t-shirt&center=rokken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/ODAwWDYwMA==/z/x5EAAOSwQItUDXvL/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/tUoAAOSwDk5UDd39/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/UYMAAOSw-jhUDeLF/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/ODAwWDYwMA==/z/x5EAAOSwQItUDXvL/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/tUoAAOSwDk5UDd39/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/UYMAAOSw-jhUDeLF/$_82.JPG',
     'price': 33.8,
     'name': 'Butterfly'
 });
 savedOutfits["top=t-shirt&center=rokken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MzY1WDQyNQ==/z/VagAAOSwd4tT0znL/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NTUwWDQxMw==/z/xeUAAOSwksVTznnP/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/2VUAAOSwPe1UDc1e/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MzY1WDQyNQ==/z/VagAAOSwd4tT0znL/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NTUwWDQxMw==/z/xeUAAOSwksVTznnP/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/2VUAAOSwPe1UDc1e/$_82.JPG',
     'price': 42,
     'name': 'blue wave'
 });
 savedOutfits["top=t-shirt&center=rokken&bottom=schoenen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MTIwWDEyMA==/z/6MMAAOSwd4tTui4X/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/MTYwWDE2MA==/$(KGrHqJHJEUFCZh0wR4SBQ),j21q0Q~~60_83.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/PmcAAOSwEK9UDdj6/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MTIwWDEyMA==/z/6MMAAOSwd4tTui4X/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/MTYwWDE2MA==/$(KGrHqJHJEUFCZh0wR4SBQ),j21q0Q~~60_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/PmcAAOSwEK9UDdj6/$_82.JPG',
     'price': 21.7,
     'name': 'Boogie'
 });
@@ -135,23 +135,23 @@ savedOutfits["top=jassen&center=jurken&bottom=sandalen"] = [];
 savedOutfits["top=jassen&center=jurken&bottom=laarzen"] = [];
 
 savedOutfits["top=jassen&center=jurken&bottom=laarzen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/VnAAAOSwDk5UDgjY/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NDAwWDQwMA==/z/SN8AAOSwVFlUDWU2/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/MTAyNFg3NjQ=/z/45sAAOSwnDZT~Gxs/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/VnAAAOSwDk5UDgjY/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NDAwWDQwMA==/z/SN8AAOSwVFlUDWU2/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/MTAyNFg3NjQ=/z/45sAAOSwnDZT~Gxs/$_82.JPG',
     'price': 59,
     'name': 'Morning spring'
 });
 savedOutfits["top=jassen&center=jurken&bottom=laarzen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg3ODc=/z/hMkAAOSw~CRTpXOz/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/ODAwWDgwMA==/z/JNMAAOSwDk5UDf6f/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/ODAwWDYwMA==/z/qx8AAOSwEK9UDgtr/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg3ODc=/z/hMkAAOSw~CRTpXOz/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/ODAwWDgwMA==/z/JNMAAOSwDk5UDf6f/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/ODAwWDYwMA==/z/qx8AAOSwEK9UDgtr/$_82.JPG',
     'price': 70.1,
     'name': 'Classy'
 });
 savedOutfits["top=jassen&center=jurken&bottom=laarzen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NTAwWDM3Mw==/z/cpUAAOSwPe1UDcGE/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/OTYwWDcyMA==/z/l-MAAOSwfcVUDduJ/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzI2WDYyMg==/$T2eC16FHJIkE9qU3iLdcBQ0G9wOYww~~60_83.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NTAwWDM3Mw==/z/cpUAAOSwPe1UDcGE/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/OTYwWDcyMA==/z/l-MAAOSwfcVUDduJ/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzI2WDYyMg==/$T2eC16FHJIkE9qU3iLdcBQ0G9wOYww~~60_82.JPG',
     'price': 48,
     'name': 'Brownie'
 });
@@ -162,23 +162,23 @@ savedOutfits["top=truien&center=broeken&bottom=sandalen"] = [];
 savedOutfits["top=truien&center=broeken&bottom=laarzen"] = [];
 
 savedOutfits["top=truien&center=broeken&bottom=laarzen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NTAwWDM3NA==/z/jOEAAOSwd4tUDhR6/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/-7kAAOSwnDZUDcBj/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/SxAAAOSwQItUDg7c/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NTAwWDM3NA==/z/jOEAAOSwd4tUDhR6/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NTAwWDM3NQ==/z/-7kAAOSwnDZUDcBj/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NzY4WDEwMjQ=/z/SxAAAOSwQItUDg7c/$_82.JPG',
     'price': 22.3,
     'name': 'Autumn'
 });
 savedOutfits["top=truien&center=broeken&bottom=laarzen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg1NzY=/z/IHMAAOSwGvhUDe9I/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/NTUwWDQxMw==/z/d1wAAOSwbqpTznlg/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NDg1WDM3NQ==/z/QiIAAOSwEK9UDfZh/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/MTAyNFg1NzY=/z/IHMAAOSwGvhUDe9I/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/NTUwWDQxMw==/z/d1wAAOSwbqpTznlg/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NDg1WDM3NQ==/z/QiIAAOSwEK9UDfZh/$_82.JPG',
     'price': 31,
     'name': 'Leather'
 });
 savedOutfits["top=truien&center=broeken&bottom=laarzen"].push({
-    'imgTop': 'http://i.marktplaats.com/00/s/NjAwWDgwMA==/z/56cAAOSwd4tUDbsu/$_81.JPG',
-    'imgCenter': 'http://i.marktplaats.com/00/s/MTAyNFg2ODA=/z/W3EAAOSwDk5T2KmN/$_81.JPG',
-    'imgBottom': 'http://i.marktplaats.com/00/s/NTAwWDM3Mw==/z/KyMAAOSwEK9UDetY/$_81.JPG',
+    'imgTop': 'http://i.marktplaats.com/00/s/NjAwWDgwMA==/z/56cAAOSwd4tUDbsu/$_82.JPG',
+    'imgCenter': 'http://i.marktplaats.com/00/s/MTAyNFg2ODA=/z/W3EAAOSwDk5T2KmN/$_82.JPG',
+    'imgBottom': 'http://i.marktplaats.com/00/s/NTAwWDM3Mw==/z/KyMAAOSwEK9UDetY/$_82.JPG',
     'price': 19.5,
     'name': 'Night out'
 });
